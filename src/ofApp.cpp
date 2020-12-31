e82ccf483cd2ec61360d1f2c9fef97b66e777e94
Tom <tombenyunes@gmail.com>
2020-12-31 15:33:54 +0000

    interpolated accelerated velocity-affected player movement

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,22 +2,32 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+	GameObject* object = new GameObject;
+	GameObjects->push_back(object);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+	for (int i = 0; i < GameObjects->size(); i++) {
+		(*GameObjects)[i]->update();
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	ofBackground(30);
+	ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
 
+	for (int i = 0; i < GameObjects->size(); i++) {
+		(*GameObjects)[i]->draw();
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+	for (int i = 0; i < GameObjects->size(); i++) {
+		(*GameObjects)[i]->keyPressed(key);
+	}
 }
 
 //--------------------------------------------------------------
@@ -32,17 +42,23 @@ void ofApp::mouseMoved(int x, int y ){
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+	for (int i = 0; i < GameObjects->size(); i++) {
+		(*GameObjects)[i]->mousePressed(x - ofGetWidth() / 2, y - ofGetHeight() / 2);
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+	for (int i = 0; i < GameObjects->size(); i++) {
+		(*GameObjects)[i]->mousePressed(x - ofGetWidth()/2, y - ofGetHeight()/2);
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+	for (int i = 0; i < GameObjects->size(); i++) {
+		(*GameObjects)[i]->mouseReleased();
+	}
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "GameObject.h"
 
 class ofApp : public ofBaseApp{
 
@@ -21,4 +22,6 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
+		vector <GameObject*> vec;
+		vector <GameObject*>* GameObjects = &vec;
 };
